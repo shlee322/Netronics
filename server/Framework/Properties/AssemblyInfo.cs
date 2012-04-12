eaa8f3d57d866aa5c9ae0e3f82f65b5ac57211e5
SangHyuck Lee <shlee322@gmail.com>
2012-04-12 22:04:22 +0900

    Version 0.1.1.21

--- server/Framework/Properties/AssemblyInfo.cs
+++ server/Framework/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      상태 번호
 //      빌드 번호
 //
-[assembly: AssemblyVersion("0.1.1.19")]
-[assembly: AssemblyFileVersion("0.1.1.19")]
+[assembly: AssemblyVersion("0.1.1.21")]
+[assembly: AssemblyFileVersion("0.1.1.21")]
